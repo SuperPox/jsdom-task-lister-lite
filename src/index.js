9cb3c8c7fb5f3454fca7c8f22de2c519632da491
SuperPox <calebkohl@gmail.com>
2021-04-15 20:54:27 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const taskForm = document.getElementById("create-task-form")
+  taskForm.addEventListener("submit", addNewTask)
+
 });
+
+
+function addNewTask(event)
+{
+  //alert ("clicked on button")
+  event.preventDefault();
+  var newTaskValue = document.getElementById("new-task-description").value;
+  var newTaskItem = document.createElement("li");
+  newTaskItem.innerText = newTaskValue;
+  
+  appendNewTask(newTaskItem);
+}
+
+function appendNewTask (value)
+{
+  const taskLine = document.getElementById("tasks");
+  taskLine.appendChild(value);
+}
+
+
+
